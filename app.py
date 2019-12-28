c5477d38284d36edf62654ca9454980f66c8fb10
willleong805850 <58291874+willleong805850@users.noreply.github.com>
2019-12-28 17:15:47 +0800

    Version 9
    modify x and y and z to loop in proper format and calculate the day by day difference of follower, following and tweets

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ from flask_restful import Resource, Api
 import datetime
 from datetime import date
 
-# creating the flask app 
-app = Flask(__name__) 
-
 #setting the api
 def start_api(consumer_key, consumer_secret,
   access_token, access_token_secret):
@@ -18,16 +15,17 @@ def start_api(consumer_key, consumer_secret,
   auth.set_access_token(access_token, access_token_secret)  
   return tweepy.API(auth)
 
-#api authentication key
+# API keys in question
 ck="DxHoukAprUS9cgUXR4yLPfy5n"
 cs="2s1fiLCR4QGdlBVkDPh0WQgGOWA6rmcFIE5wzY5nYa1AsSayn2"
 at="1207894049106956288-vVSh9pHeHm0GRKRXTNxnVVM9Ep1aiD"
 ats="FY40u4cNBixTBzl5n9lH4847IGz5vCbHSBbYH9BNrK2g1"
-
 my_api = start_api(ck,cs,at,ats)
 
+# creating the flask app 
+app = Flask(__name__) 
+
 def get_user_info(api, name):
-    
   user = api.get_user(screen_name = name) 
   return {"username": user.name,
             "creation date": user.created_at.strftime('%Y-%m-%d'),
@@ -70,52 +68,53 @@ class Twitter(db.Model): # create table twitter
         self.like_count = like_count
         
 def insert_data(name):
-  
+  # this line is for actualy getting data from the API
   info = get_api_info(name)#get_api_info
   # try to separate the value that the api returns, but can't get them when i was using app.route, so that i can insert it into mysql database
   today = date.today()
   exists = db.session.query(Account_created.id).filter_by(username=user).scalar() is not None # exists tests if the user exists in the database or not
   dateexist = db.session.query(Twitter.id).filter_by(DATE = today, username = user).scalar() is not None # dateexist tests if the current datapoint exitss in the database or not
-  print (exists);
-  print(dateexist);
+  #print (exists);
+  #print(dateexist);
 
   if(exists == False):# create new information
-   data = Account_created(user,cre)
-   db.session.add(data)
-   db.session.commit()
-   # the next three lines are used to add data into the database
-   insdata = Twitter(user,today.strftime('%Y-%m-%d'),followers,following,tweet,like)
-   db.session.add(insdata)
-   db.session.commit()
+    
+    data = Account_created(user,cre)
+    db.session.add(data)
+    db.session.commit()
+    # the next three lines are used to add data into the database
+    insdata = Twitter(user,today.strftime('%Y-%m-%d'),followers,following,tweet,like)
+    db.session.add(insdata)
+    db.session.commit()
     
   elif(dateexist == False):#add a new day data
    insdata = Twitter(user,today.strftime('%Y-%m-%d'),followers,following,tweet,like)
    db.session.add(insdata)
    db.session.commit()
   
-  elif(dateexist == True):#update query when there has changes that day
+  elif(dateexist == True):#update query when there has changes in that day data
    updatedata = Twitter.query.filter_by(username = user, DATE = today).update({"followers_count": (followers), "following_count": (following), "tweet_count": (tweet), "like_count": (like)})
    db.session.commit()
+    
   # the followine line is used to get data from the database
   r = db.engine.execute('select username,DATE,followers_count,following_count,tweet_count,like_count from Twitter where username = "' + user + '" ORDER BY DATE DESC')
-
-  x = [(a,b.strftime('%Y-%m-%d'),c,d,e,f) for (a,b,c,d,e,f) in r]
-
-  y = [(x[i][2]-x[i-1][2],x[i][3]-x[i-1][3],x[i][4]-x[i-1][4])for i in range(len(x)-1)] + [(0,0,0)]
-
-  z = [(x[i][0], x[i][1],
-    x[i][2], y[i][0],
-    x[i][3], y[i][1],
-    x[i][4], y[i][2])
-    for i in range(len(x))]
-
-  print (z)
+  
+  # in here, x, y and z are the partial return values
+  x = [{"date":i[1].strftime('%Y-%m-%d'),"followers":i[2],"following":i[3],"tweets":i[4]} for i in r] #just loop 
+  
+  y = [{"followers_diff":x[i]["followers"]-x[i+1]["followers"],
+        "following_diff":x[i]["following"]-x[i+1]["following"],
+        "tweets_diff":x[i]["tweets"]-x[i+1]["tweets"]}
+        for i in range(len(x)-1)] + [{"followers_diff":0,"following_diff":0,"tweets_diff":0}]
+  
+  z = [{**x[i], **y[i]} for i in range(len(x))]
+  # finally we are returning the data to the API
+  return (flask.jsonify({"name": info["username"], "cre": info["creation date"],"like": info["like count"], "table": z}))
 
 # make a new function here with a decorator implies an internal variable called "name"
 @app.route('/<name>')
 def flask_json(name):
     return insert_data(name)
 
-# driver function 
 if __name__ == '__main__':
     app.run(debug=True)
